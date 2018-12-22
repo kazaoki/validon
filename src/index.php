a73467c0dcc461e4c5bf80f3d09ada25edd8c7d6
Tatsuya Karino <karino@kazaoki.jp>
2018-12-23 00:45:01 +0900

    [UPDATE] 途中

--- src/index.php
+++ src/index.php
@@ -8,8 +8,12 @@
 </head>
 <body>
 
-<form action="check.php" method="post">
-
+<form action="check.php" method="post"
+  data-validon-config="contact/01"
+  data-validon-fire="blur"
+  data-validon-errortag="<div class=error>$message</div>"
+  data-validon-position="append"
+>
   お名前：<input type="text" name="name"><br>
   カナ：<input type="text" name="kana"><br>
   年齢：<input type="text" name="age"><br>
